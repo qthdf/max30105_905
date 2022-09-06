b1283d8a478a1a50730338245ab56d8847367db1
qthdf <110314206+qthdf@users.noreply.github.com>
2022-09-06 11:13:54 +0800

    Update heart.cpp

--- heart.cpp
+++ heart.cpp
@@ -18,37 +18,27 @@
 #include <cstdint>
 #include <math.h>
 #include "MAX30105.h"
-#include "spo2_algorithm.h"
-enum LEDToRead {
-    //% block="红光" enumval=1
-    Red = 1,
-    //% block="红外线" enumval=2
-    Infrared = 2,
-    };
+#include "algorithm.h"
+enum LEDToRead{
+	//% block="Red" enumval=1
+	Red=1,	
+	//% block="Infrared" enumval=2
+	Infrared=2,
+};
+ 
+enum HeartbeatType{
+	//% block="BPM" enumval=0
+	BPM=0,
+	//% block="AVG" enumval=1
+	AVG=1,
+};
 
-
-enum HeartbeatType {
-    //% block="瞬时心率" enumval=0
-    BPM = 0,
-    //% block="平均心率" enumval=1
-    AVG = 1,
-    };
-    
-    
-enum Spo2Type {
-    //% block="瞬时血氧" enumval=0
-    S1 = 0,
-    //% block="平均血氧" enumval=1
-    S2 = 1,
-    };
-
-
-enum LEDMode {
-    //% block="红光" enumval=2
-    RedLED = 2,
-    //% block="红光和红外线" enumval=3
-    RedAndIR = 3,
-    };
+enum LEDMode{
+	//% block="Red" enumval=2
+	RedLED=2,
+	//% block="Red & Infrared" enumval=3
+	RedAndIR=3,
+};
 
 using namespace pxt;
 
@@ -67,7 +57,7 @@ namespace Microbit {
 	uint32_t irBuffer[100]; //infrared LED sensor data
 	uint32_t redBuffer[100];  //red LED sensor data
 	int32_t bufferLength; //data length
-	int32_t spo2values; //SPO2 value
+	int32_t spo2_value; //SPO2 value
 	int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
 	int32_t heartRate; //heart rate value
 	int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
@@ -172,14 +162,14 @@ namespace Microbit {
 			irBuffer[i] = particleSensor->getIR();
 			particleSensor->nextSample(); //We're finished with this sample so move to next sample
 		}
-		maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2values, &validSPO2, &heartRate, &validHeartRate);
+		maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2_value, &validSPO2, &heartRate, &validHeartRate);
 		while (1)
 		{
 			//dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
 			for (byte i = 25; i < 100; i++)
 			{
-				redBuffer[i - 25] = redBuffer[i];
-				irBuffer[i - 25] = irBuffer[i];
+			redBuffer[i - 25] = redBuffer[i];
+			irBuffer[i - 25] = irBuffer[i];
 			}
 
 			//take 25 sets of samples before calculating the heart rate.
@@ -193,14 +183,13 @@ namespace Microbit {
 				irBuffer[i] = particleSensor->getIR();
 				particleSensor->nextSample(); //We're finished with this sample so move to next sample
 				//After gathering 25 new samples recalculate HR and SP02
-				
+				maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2_value, &validSPO2, &heartRate, &validHeartRate);
 			}
-			maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2values, &validSPO2, &heartRate, &validHeartRate);
 		}
 			switch(type)
 			{
 				case 0:
-					myspo2 = (uint8_t)spo2values;
+					myspo2 = (uint8_t)spo2_value;
 					break;
 					
 				case 1:
@@ -210,5 +199,13 @@ namespace Microbit {
 			}
 		return myspo2;
 	}
-	
+
+
+
+
+
+
 }
+
+
+
